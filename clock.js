546582edac0cc922ad7f0e75766781148a295ebf
JoeTheorium <fernandoandresraggio@gmail.com>
2023-04-29 23:55:21 -0300

    Clock

--- clock.js
+++ clock.js
@@ -1,5 +1,14 @@
 const clock = document.getElementById("clock");
 
 function setDate() {
-    
+    const now = new Date();
+    const secondsAngle = now.getSeconds() * 6;
+    const minsAngle = now.getMinutes() * 6 + secondsAngle / 60;
+    const hourAngle = ((now.getHours() % 12) / 12) * 360 + minsAngle / 12;
 }
+
+
+
+setInterval(setDate, 1000);
+
+setDate();
