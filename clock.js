d4c4a84586131ee89d81c6995abff16c543d2bf5
JoeTheorium <fernandoandresraggio@gmail.com>
2023-05-01 18:40:32 -0300

    Clock

--- clock.js
+++ clock.js
@@ -4,17 +4,18 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setClock() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const secsDeg = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secsDeg}deg)`;
 
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const minsDeg = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minuteHand.style.transform = `rotate(${minsDeg}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    const hrsDeg = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hrsDeg}deg)`;
 }
 
 setInterval(setClock, 1000);
