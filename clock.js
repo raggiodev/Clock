8ff48067a8bbb47c39e1172328ac2374d08e0bf5
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-05-02 21:05:24 -0300

    Clock
    Clock.

--- clock.js
+++ clock.js
@@ -8,15 +8,15 @@ function setClock() {
 
     const seconds = now.getSeconds();
     const secsDeg = seconds * (360 / 60);
-    secondHand.style.transform = rotate(${secsDeg}deg);
+    secondHand.style.transform = `rotate(${secsDeg}deg)`;
 
     const minutes = now.getMinutes();
     const minsDeg = minutes * (360 / 60) + (seconds * (360 / 60) / 360);
-    minuteHand.style.transform = rotate(${minsDeg}deg);
+    minuteHand.style.transform = `rotate(${minsDeg}deg)`;
 
     const hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
     const hrsDeg = hours * (360 / 12) + (minutes * (360 / 60) / 360 ) + (seconds * (360 / 60) / 360);
-    hourHand.style.transform = rotate(${hrsDeg}deg);
+    hourHand.style.transform = `rotate(${hrsDeg}deg)`;
 }
 
 setInterval(setClock, 1000);
